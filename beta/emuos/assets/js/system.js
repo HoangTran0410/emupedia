59cea204600d99ec15ac9a118cf263c003a34d8b
Hoang Tran - mac pro <99.hoangtran@gmail.com>
2025-12-06 11:31:07 +0700

    fix relative path

--- beta/emuos/assets/js/system.js
+++ beta/emuos/assets/js/system.js
@@ -987,7 +987,7 @@
 				default:
 					el = global.document.createElement('script');
 					el.type = typeof type === 'string' ? type : 'text/javascript';
-					el.src = 'assets/js/' + url + '.js';
+					el.src = './assets/js/' + url + '.js';
 					el.async = false;
 					break;
 			}
